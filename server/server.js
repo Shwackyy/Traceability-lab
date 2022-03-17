43b2f3fd73d3275e406aaed6ca1d0269fb5b6c36
Brett Stevens <brettstevens899@yahoo.com>
2022-03-17 14:29:54 -0600

    trace

--- server/server.js
+++ server/server.js
@@ -4,12 +4,13 @@ const path = require('path')
 const cors = require('cors')
 
 app.use(express.json())
-app.use(cors())
+app.use(cors());
 
-const port = process.env.PORT || 4005;
+
+
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
     console.log(`We are online on ${port}`)
 });
-
 app.use(express.static(path.join(__dirname, "../handouts")));

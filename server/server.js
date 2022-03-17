4793e2215a42dd933124dd076d2d20f82768333e
Brett Stevens <brettstevens899@yahoo.com>
2022-03-17 14:54:48 -0600

    trace

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "../Public")));
 
-
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
     console.log(`We are online on ${port}`)
 });
-app.use(express.static(path.join(__dirname, "../Public")));
-
